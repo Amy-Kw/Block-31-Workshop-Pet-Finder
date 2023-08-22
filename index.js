0c910a1dfb45a61b3288c4de890a1f3a95fbe134
Amy KWong <amykwong6@gmail.com>
2023-08-22 06:41:06 -0400

    owner and pet name

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = 9090;
 
 // GET - / - returns homepage
 app.get("/", (req, res) => {
-  res.send("homepage"); //like this?
+  //   res.send("homepage"); //like this?
   // serve up the public folder as static index.html file
 });
 
@@ -22,35 +22,44 @@ app.get("/api", (req, res) => {
 // get all pets from the database
 app.get("/api/v1/pets", (req, res) => {
   // send the pets array as a response
-  res.send(pets);
+  res.json(pets);
 });
 
 // get pet by owner with query string
-app.get("/api/v1/pets/owner/:owner", (req, res) => {
+app.get("/api/v1/pets/owner/:owner", function (req, res) {
   // get the owner from the request
-  const { owner } = req.params;
+  console.log("owner: " + req.query.owner);
+  //   const { owner } = req.params
   // find the pet in the pets array
-  const pet = pets.find(
-    (pet) => pet.owner.toLowerCase() == owner.toLowerCase()
-  );
+  //   const pet = pets.find(
+  //     pet => pet.owner.toLowerCase() == owner.toLowerCase()
+  //   );
+  //when I save the pet part auto correct to have (pet)
   // send the pet as a response
-  res.send(owner);
+  res.send("owner: " + req.query.owner);
 });
 
 // get pet by name  // not working
-app.get("/api/v1/pets/:name", (req, res) => {
+app.get("/api/v1/pets/:name", function (req, res) {
   // get the name from the request
-  const { name } = req.params;
+  //   const { name } = req.params;
+  console.log("name: " + req.query.owner);
 
   // find the pet in the pets array
-  const pet = pets.find((pet) => pet.name.toLowerCase() === name.toLowerCase());
+  //   const pet = pets.find((pet) => pet.name === name);
 
   // send the pet as a response
-  res.send(pet);
+  //   res.send(pet);
+  res.send("name: " + req.query.owner);
 });
 
-app.listen(PORT, () => {
-  console.log("Server is listening on port " + PORT);
+// app.listen(PORT, () => {
+//   console.log("Server is listening on port " + PORT);
+// });
+
+app.listen(PORT, function (err) {
+  if (err) console.log(err);
+  console.log("Server is listening on port ", PORT);
 });
 
 module.exports = app;
