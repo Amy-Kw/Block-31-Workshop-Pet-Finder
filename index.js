6cc037e56b0b6c3804ee7ded4eea56fbbe52f2d5
Amy KWong <amykwong6@gmail.com>
2023-08-22 05:21:27 -0400

    pet,hello word, ad api working

--- index.js
+++ index.js
@@ -1,55 +1,56 @@
 // import the pets array from data.js
-const pets = require('./data');
+
+const pets = require("./data");
 
 // init express app
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const PORT = 8080;
+const PORT = 9090;
 
 // GET - / - returns homepage
-app.get('/', (req, res) => {
-    // serve up the public folder as static index.html file
-
+app.get("/", (req, res) => {
+  res.send("homepage"); //like this?
+  // serve up the public folder as static index.html file
 });
 
 // hello world route
-app.get('/api', (req, res) => {
-    res.send('Hello World!');
+app.get("/api", (req, res) => {
+  res.send("Hello World!");
 });
 
 // get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
-    // send the pets array as a response
-
+app.get("/api/v1/pets", (req, res) => {
+  // send the pets array as a response
+  res.send(pets);
 });
 
 // get pet by owner with query string
-app.get('/api/v1/pets/owner', (req, res) => {
-    // get the owner from the request
-
-
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.owner === owner);
-
-    // send the pet as a response
-
+app.get("/api/v1/pets/owner/:owner", (req, res) => {
+  // get the owner from the request
+  const { owner } = req.params;
+  // find the pet in the pets array
+  const pet = pets.find(
+    (pet) => pet.owner.toLowerCase() == owner.toLowerCase()
+  );
+  // send the pet as a response
+  res.send(owner);
 });
 
-// get pet by name
-app.get('/api/v1/pets/:name', (req, res) => {
-    // get the name from the request
+// get pet by name  // not working
+app.get("/api/v1/pets/:name", (req, res) => {
+  // get the name from the request
+  const { name } = req.params;
 
+  // find the pet in the pets array
+  const pet = pets.find((pet) => pet.name.toLowerCase() === name.toLowerCase());
 
-    // find the pet in the pets array
-    const pet = pets.find(pet => pet.name === name);
-
-    // send the pet as a response
-
+  // send the pet as a response
+  res.send(pet);
 });
 
 app.listen(PORT, () => {
-    console.log('Server is listening on port ' + PORT);
+  console.log("Server is listening on port " + PORT);
 });
 
 module.exports = app;
