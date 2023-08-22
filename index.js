0c39bc0a9cc159deaf04c77360f3dbde56bff74c
Amy KWong <amykwong6@gmail.com>
2023-08-22 12:44:53 -0400

    updating

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ const PORT = 9090;
 
 // GET - / - returns homepage
 app.get("/", (req, res) => {
-  //   res.send("homepage"); //like this?
   // serve up the public folder as static index.html file
+  res.sendfile(__dirname + "/public/index.html");
 });
 
 // hello world route
@@ -22,44 +22,30 @@ app.get("/api", (req, res) => {
 // get all pets from the database
 app.get("/api/v1/pets", (req, res) => {
   // send the pets array as a response
-  res.json(pets);
+  res.send(pets);
 });
 
-// // get pet by owner with query string
-// app.get("/api/v1/pets/owner/:owner", function (req, res) {
-//   // get the owner from the request
-//   console.log("owner: " + req.query.owner);
-//   //   const { owner } = req.params
-//   // find the pet in the pets array
-//   //   const pet = pets.find(
-//   //     pet => pet.owner.toLowerCase() == owner.toLowerCase()
-//   //   );
-//   //when I save the pet part auto correct to have (pet)
-//   // send the pet as a response
-//   res.send("owner: " + req.query.owner);
-// });
-
 // get pet by owner with query string
-app.get("/api/v1/pets/owner", (req, res) => {
-  const { owner } = req.params;
-
+app.get("/api/v1/pets/owner", function (req, res) {
   // get the owner from the request
-
+  //   console.log("owner: " + req.query.owner);
+  const { owner } = req.query;
   // find the pet in the pets array
-  const pet = pets.find((pet) => pet.owner === owner);
-
+  //   const pet = pets.find(
+  const pet = pets.filter((pet) => pet.owner === owner); //when I save the pet part auto correct to have (pet), i know its (pet....
   // send the pet as a response
   res.send(pet);
+  //   res.send("owner: " + req.query.owner);
 });
 
 // get pet by name
 app.get("/api/v1/pets/:name", function (req, res) {
   // get the name from the request
-  //   const { name } = req.params;
+  //   const name = req.params,name;
   console.log("name: " + req.query.owner);
 
   // find the pet in the pets array
-  //   const pet = pets.find((pet) => pet.name === name);
+  //   const pet = pets.find(pet => pet.name.toLowerCase() === name.toLowerCase());
 
   // send the pet as a response
   //   res.send(pet);
